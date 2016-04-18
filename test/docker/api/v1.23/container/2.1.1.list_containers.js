7b64433edea53c7766f33fb8dbd5c99c8cc4db96
azreas <azreas.x@gmail.com>
2016-04-18 15:02:48 +0800

    for test

--- test/docker/api/v1.23/container/2.1.1.list_containers.js
+++ test/docker/api/v1.23/container/2.1.1.list_containers.js
@@ -1,3 +1,13 @@
 /**
  * Created by feidD on 2016/4/18.
  */
+var rest = require('restler');
+
+rest.get('http://192.168.1.238:2375/containers/json').on('complete', function(result) {
+    if (result instanceof Error) {
+        console.log('Error:', result.message);
+        this.retry(5000); // try again after 5 sec
+    } else {
+        console.log(result);
+    }
+});
