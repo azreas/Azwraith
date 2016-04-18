6d67d3a329f8ba0ae8c9330cc3b53116385fc85b
zerolinke <zerolinke@gmail.com>
2016-04-18 15:27:07 +0800

    write dockerapitest host in setting

--- test/docker/api/v1.23/container/2.1.1.list_containers.js
+++ test/docker/api/v1.23/container/2.1.1.list_containers.js
@@ -2,8 +2,9 @@
  * Created by feidD on 2016/4/18.
  */
 var rest = require('restler');
+var dockerapitest=require('../../../../../settings').dockerapitest;
 
-rest.get('http://192.168.1.238:2375/containers/json').on('complete', function(result) {
+rest.get('http://'+dockerapitest.host+':'+dockerapitest.port+'/containers/json').on('complete', function(result) {
     if (result instanceof Error) {
         console.log('Error:', result.message);
         this.retry(5000); // try again after 5 sec
