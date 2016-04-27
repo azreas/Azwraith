cfc8c76ef31a995f42294e837a4c9a0048fd0a97
zhechu <ling-yu-wang@qq.com>
2016-04-27 18:54:02 +0800

    日志添加 logs 文件夹 判断

--- modules/log/log.js
+++ modules/log/log.js
@@ -3,6 +3,18 @@
  * Created by lingyuwang on 2016/4/27.
  */
 
+var fs = require('fs');
+
+var path = require('path');
+
+var logs_dir = path.join(__dirname, "../../logs");
+// 检查 logs 文件夹是否存在，不存在则自动生成
+console.log("logs_dir ---> "+logs_dir);
+if (!fs.existsSync(logs_dir)){
+    console.log("还没创建 logs");
+    fs.mkdirSync(logs_dir);
+}
+
 //module.exports = require("./log_db");
 module.exports = require("./log_file");
 
