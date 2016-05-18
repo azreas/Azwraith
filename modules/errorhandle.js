c5134bccdb319d4fdf735f33f6ad93c0d6127081
GreatGreatGreatMagincianHC <2330477790@qq.com>
2016-05-18 15:35:39 +0800

    加入500页面，加入404，500错误捕捉及跳转相对应页面

--- modules/errorhandle.js
+++ modules/errorhandle.js
@@ -3,9 +3,9 @@
  * Created by lingyuwang on 2016/3/25.
  */
 
-module.exports = function(app){
+module.exports = function (app) {
     // 404
-    app.use(function(req, res, next) {
+    app.use(function (req, res, next) {
         var err = new Error('Not Found');
         err.status = 404;
         next(err);
@@ -13,22 +13,24 @@ module.exports = function(app){
 
     // 500
     if (app.get('env') === 'development') {
-        app.use(function(err, req, res, next) {
-            res.status(err.status || 500);
-            res.render('error', {
-                message: err.message,
-                error: err
-            });
+        app.use(function (err, req, res, next) {
+            if (err.status == 404) {
+                res.render('404', {});
+            } else {
+                res.status(err.status || 500);
+                res.render('500', {});
+            }
         });
     }
 
     // error
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: {}
-        });
+    app.use(function (err, req, res, next) {
+        if (err.status == 404) {
+            res.render('404', {});
+        } else {
+            res.status(err.status || 500);
+            res.render('500', {});
+        }
     });
 }
 
